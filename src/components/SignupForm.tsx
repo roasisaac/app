86e4b3035af497afaf1a5cf8c5da819c17ab8bd5
Shawaiz Khan <shawaizkhan.biz@gmail.com>
2024-09-24 19:34:07 +0100

    update: Updated header based on login status

--- src/components/SignupForm.tsx
+++ src/components/SignupForm.tsx
@@ -1,15 +1,20 @@
 import React from 'react'
+import Header from './Header';
 
 const SignupForm: React.FC = () => {
     return (
-        <div className='bg-white'>
-            <form action="#">
-                <div>
-                    <input type="text" placeholder='Name' />
-                    <input type="text" placeholder='Email' />
-                </div>
-            </form>
-        </div>
+        <>
+            <Header />
+            <div className='bg-white flex justify-center items-center min-h-screen w-full'>
+                <form action="#">
+                    <div className='bg-gray-300 w-full'>
+                        <input type="text" placeholder='Name' />
+                        <input type="text" placeholder='Email' />
+                        <input type="text" placeholder='Email' />
+                    </div>
+                </form>
+            </div>
+        </>
     );
 }
 
