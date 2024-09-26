e791285a5f114c1ac1d659c5be5dce22c3e0ef54
Shawaiz Khan <shawaizkhan.biz@gmail.com>
2024-09-26 16:35:43 +0100

    error: Process .env issue

--- src/test/TestComponent.tsx
+++ src/test/TestComponent.tsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 
 const TestComponent = () => {
     useEffect(() => {
-        console.log("API Key:", 1);
+        console.log("API Key: ", process.env.REACT_APP_API_KEY);
     }, []);
 
     return <div>Check console for API Key</div>;
